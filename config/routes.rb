a0aaa803139abdd2481551f1c33daef461161f26
Larry Goodman <lgoodman@Larry-Goodman-C02FP240Q05Q.local>
2022-08-16 15:12:00 -0400

    Add CRUD capabilities and Comment model

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   root "articles#index"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
-  
-
-  
+  resources :articles
 end
